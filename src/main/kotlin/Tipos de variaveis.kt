11515238c9af2081e9645065f47f88a1b9508fb6
Thamiles Carvalho <thamiles.085@gmail.com>
2024-01-21 15:59:34 -0300

    Controle de Fluxo - Tipos de Variáveis

---
+++ src/main/kotlin/Tipos de variaveis.kt
@@ -0,0 +1,29 @@
+fun main() {
+    var x: Int = 10 // variavel mutavel
+    val y: Int = 11 // variavel imutavel
+    var a: Float = 29.03f
+    var b: Double = 6.57889
+    var c: Long = 131456464845361361
+    var d: Char = 'o' // Texto de 1 letra
+    var e: String = "Thamiles" // Texto normal
+    var f: Short = 100
+    var g: Byte = 0b1010
+    var h: Byte = 0xf
+    var i: UInt = 96u // Unsigned
+
+    println("Tipos de váriaveis")
+
+    println("Var(mutável) Int(inteiro) : "+x)
+    println("Val(imutável) Int(inteiro) : "+y)
+    println("Float: "+a)
+    println("Double: "+b)
+    println("Long: "+c)
+    println("Char: "+d)
+    println("String: "+e)
+    println("Short: "+f)
+    println("Byte: "+g)
+    println("Byte: "+h)
+    println("UInt: "+i)
+
+
+}
