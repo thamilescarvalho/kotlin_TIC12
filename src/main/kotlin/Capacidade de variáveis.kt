4fabf85d8840934f53d20ae042276bbc9c26b6f9
Thamiles Carvalho <thamiles.085@gmail.com>
2024-01-21 16:52:05 -0300

    Controle de Fluxo - Capacidade de variáveis 2.0

--- src/main/kotlin/Capacidade de variáveis.kt
+++ src/main/kotlin/Capacidade de variáveis.kt
@@ -2,7 +2,6 @@ fun main() {
     println("ULong máximo: ${ULong.MAX_VALUE} ULong mínimo: ${ULong.MIN_VALUE}")
     println("UInt máximo: ${UInt.MAX_VALUE} UInt mínimo: ${UInt.MIN_VALUE}")
     println("UShort máximo: ${UShort.MAX_VALUE} UShort mínimo: ${UShort.MIN_VALUE}")
-    println("Long máximo: ${Long.MAX_VALUE} Long mínimo: ${
-        .MIN_VALUE}")
+    println("Long máximo: ${Long.MAX_VALUE} Long mínimo: ${Long.MIN_VALUE}")
 
 }
