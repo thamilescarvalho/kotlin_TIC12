4bc4fbedfa85305cf52141912c912da6a361ea5b
Thamiles Carvalho <thamiles.085@gmail.com>
2024-01-22 08:22:41 -0300

    Controle de Fluxo - Variáveis mutáveis e imutáveis

---
+++ src/main/kotlin/Variáveis_2.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var t:Int = 29
+    var str:String = "Eu"
+
+    t++
+    str+= " Te Amo!"
+
+    println("Imprimindo o valor da variável t acrescentada de ++: ${t}")
+    println("Imprimindo o valor da variável str acrescentada de +: ${str}")
+}
