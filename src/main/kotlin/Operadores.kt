0f13eef4e05bf24948fcce1e02008f4d4d48d533
Thamiles Carvalho <thamiles.085@gmail.com>
2024-01-22 08:47:46 -0300

    Controle de Fluxo - Operadores

---
+++ src/main/kotlin/Operadores.kt
@@ -0,0 +1,42 @@
+fun main() {
+    var h:Boolean = false
+    h = !(h)
+    println(h)
+
+    println((3+2)*5)
+    println(3+2*5)
+
+}
+
+/*
+____________________________
+
+*  OPERADORES LÓGICOS:
+*
+*     && = e
+*     !! = OU
+*      ! = negação
+*
+* ____________________________
+*
+*  OPERADORES MATEMÁTICOS:
+*
+*      * = adição
+*      - = subtração
+*      * = multiplicação
+*      / = divisão
+*      % = resto da divisão
+*
+*  ____________________________
+*
+*  OPERADORES COMPARATIVOS:
+*
+*      > = maior que
+*     >= = maior que ou igual
+*      < = menor
+*     <= = menor que ou igual
+*     == = igualdade voltada a comparação
+*     =  = atribuição
+*     != = diferente
+*
+* */
