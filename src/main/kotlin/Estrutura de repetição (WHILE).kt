f4afb01bbcde8f5f24e2463839a60f5320402d2d
Thamiles Carvalho <thamiles.085@gmail.com>
2024-01-24 13:17:23 -0300

    Controle de Fluxo - Estrutura de repetição (WHILE)

---
+++ src/main/kotlin/Estrutura de repetição (WHILE).kt
@@ -0,0 +1,20 @@
+fun main() {
+    var i=0
+        while(i<=10){
+            print("${i} ")
+            i++
+        }
+    print("\n")
+        while (i>=0){
+            print("${i} ")
+            i--
+        }
+    print("\n")
+    var str = "TIC 12"
+    var j=0
+        while(j<str.length){
+            print("${j} ")
+            j++
+        }
+
+}
