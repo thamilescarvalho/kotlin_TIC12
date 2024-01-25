8c008f9847a1cfa833802978cf681be88eef7ed7
Thamiles Carvalho <thamiles.085@gmail.com>
2024-01-25 14:36:02 -0300

    Controle de Fluxo - Chamadas entre funções

---
+++ src/main/kotlin/Casa.kt
@@ -0,0 +1,23 @@
+fun apartamento() {
+    println("Entrando no meu apartamento..")
+    cozinha()
+}
+fun cozinha() {
+    println("Entrando na cozinha..")
+    geladeira()
+}
+fun geladeira() {
+    println("Abrindo a geladeira..")
+    creme_de_abacaxi()
+}
+fun creme_de_abacaxi() {
+    println("Comendo o creme de abacaxi..")
+    agua()
+}
+fun agua() {
+    println("Bebendo água..")
+}
+
+fun main() {
+    apartamento()
+}
